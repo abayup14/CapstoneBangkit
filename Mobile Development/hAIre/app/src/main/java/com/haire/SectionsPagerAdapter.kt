fb9ec0d91bcb4a37846144964b4f74a9c56c6597
palkon-code <120707573+palkon-code@users.noreply.github.com>
2023-11-20 16:09:09 +0700

    Extract MD files

---
+++ Mobile Development/hAIre/app/src/main/java/com/haire/SectionsPagerAdapter.kt
@@ -0,0 +1,21 @@
+package com.haire
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class SectionsPagerAdapter(activity: AppCompatActivity) : FragmentStateAdapter(activity) {
+    override fun getItemCount(): Int {
+        return 2
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        var fragment: Fragment? = null
+        when (position) {
+            0 -> fragment = OpenJobsFragment()
+            1 -> fragment = StatusFragment()
+        }
+        return fragment as Fragment
+    }
+
+}
