96610f90286430f17b254d6d774bc04e8133bae1
abayup14 <abayup1498@gmail.com>
2023-12-04 07:54:14 +0700

    Tambah Coba2 untuk GraphQL bukan menggunakan project ini

---
+++ Machine Learning/GraphQL/api/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+CORS(app)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:@localhost:3306/trying"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+app.app_context().push()
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
