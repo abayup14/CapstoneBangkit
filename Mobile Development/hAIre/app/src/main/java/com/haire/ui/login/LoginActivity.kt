bea2c329dd7d71d1b4759be0693776e7352b07c8
ilham34 <ilhamwidi34@gmail.com>
2023-12-08 17:18:27 +0700

    login dan register complete

--- Mobile Development/hAIre/app/src/main/java/com/haire/ui/login/LoginActivity.kt
+++ Mobile Development/hAIre/app/src/main/java/com/haire/ui/login/LoginActivity.kt
@@ -2,8 +2,14 @@ package com.haire.ui.login
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.haire.MainActivity
 import com.haire.R
 import com.haire.databinding.ActivityLoginBinding
@@ -12,6 +18,7 @@ import com.haire.ui.register.RegisterActivity
 class LoginActivity : AppCompatActivity() {
     private var _binding: ActivityLoginBinding? = null
     private val binding get() = _binding!!
+    private lateinit var dbUser: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,19 +27,54 @@ class LoginActivity : AppCompatActivity() {
         supportActionBar?.hide()
 
         binding.btnLogin.setOnClickListener {
-            AlertDialog.Builder(this).apply {
-                setMessage(getString(R.string.login_success))
-                setTitle("Login")
-                setPositiveButton(getString(R.string.yes)) { _, _ ->
-                    startActivity(Intent(this@LoginActivity, MainActivity::class.java))
-                    finish()
-                }
-                show()
+            val email = binding.emailEditText.text.toString().replace(".", ",")
+            val password = binding.passwordEditText.text.toString()
+            dbUser = FirebaseDatabase.getInstance().getReference("users")
+
+            if (email.isEmpty() || password.isEmpty()) {
+                showText("Email or password still empty")
+            } else {
+                dbUser.addListenerForSingleValueEvent(object : ValueEventListener {
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        if (snapshot.child(email).exists()) {
+                            if (snapshot.child(email).child("password").getValue(String::class.java)
+                                    .equals(password)
+                            ) {
+                                AlertDialog.Builder(this@LoginActivity).apply {
+                                    setMessage(getString(R.string.login_success))
+                                    setTitle("Login")
+                                    setPositiveButton(getString(R.string.yes)) { _, _ ->
+                                        startActivity(
+                                            Intent(
+                                                this@LoginActivity,
+                                                MainActivity::class.java
+                                            )
+                                        )
+                                        finish()
+                                    }
+                                    show()
+                                }
+                            } else {
+                                showText("Password is not correct")
+                            }
+                        } else {
+                            showText("Data belum terdaftar")
+                        }
+                    }
+
+                    override fun onCancelled(error: DatabaseError) {
+                        showText(error.message)
+                    }
+                })
             }
         }
         binding.tvRegister.setOnClickListener {
             startActivity(Intent(this@LoginActivity, RegisterActivity::class.java))
             finish()
         }
+    }
+
+    private fun showText(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
--- Mobile Development/hAIre/app/src/main/java/com/haire/ui/register/RegisterActivity.kt
+++ Mobile Development/hAIre/app/src/main/java/com/haire/ui/register/RegisterActivity.kt
@@ -1,10 +1,13 @@
 package com.haire.ui.register
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RadioButton
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
-import com.haire.MainActivity
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.haire.R
 import com.haire.databinding.ActivityRegisterBinding
 import com.haire.ui.login.LoginActivity
@@ -13,23 +16,74 @@ class RegisterActivity : AppCompatActivity() {
     private var _binding: ActivityRegisterBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var dbUser: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportActionBar?.hide()
 
+        dbUser = FirebaseDatabase.getInstance()
+            .getReferenceFromUrl("https://haire-test-de01d-default-rtdb.firebaseio.com/")
+
         binding.btnRegister.setOnClickListener {
-            AlertDialog.Builder(this).apply {
-                setMessage(getString(R.string.register_success))
-                setTitle("Register")
-                setPositiveButton(getString(R.string.login)) { _, _ ->
-                    startActivity(Intent(this@RegisterActivity, LoginActivity::class.java))
-                    finish()
+            val name = binding.edtName.text.toString()
+            val email = binding.edtEmail.text.toString().replace(".", ",")
+            val pass = binding.edtPassword.text.toString()
+            val rePass = binding.edtRePassword.text.toString()
+            val phone = binding.edtPhone.text.toString()
+            val homelessIsChecked = binding.homelessRg.checkedRadioButtonId
+            val disabledIsChecked = binding.disabledRg.checkedRadioButtonId
+            var homeless = false
+            var disabled = false
+
+            binding.homelessRg.setOnCheckedChangeListener { _, checkedId ->
+                val selectedRadioButton: RadioButton = findViewById(checkedId)
+                when (selectedRadioButton.text.toString()) {
+                    "yes" -> homeless = false
+                    "no" -> homeless = true
                 }
-                show()
+            }
+
+            binding.disabledRg.setOnCheckedChangeListener { _, checkedId ->
+                val selectedRadioButton: RadioButton = findViewById(checkedId)
+                when (selectedRadioButton.text.toString()) {
+                    "yes" -> disabled = true
+                    "no" -> disabled = false
+                }
+            }
+
+            if (name.isEmpty() || email.isEmpty() || pass.isEmpty() || rePass.isEmpty() || phone.isEmpty() || homelessIsChecked == -1 || disabledIsChecked == -1) {
+                Toast.makeText(
+                    this@RegisterActivity,
+                    "There's still empty field",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else if (pass != rePass) {
+                Toast.makeText(this@RegisterActivity, "Password didn't match", Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                dbUser = FirebaseDatabase.getInstance().getReference("users")
+                dbUser.child(email).child("name").setValue(name)
+                dbUser.child(email).child("email").setValue(email)
+                dbUser.child(email).child("phone").setValue(phone)
+                dbUser.child(email).child("password").setValue(pass)
+                dbUser.child(email).child("isHomeless").setValue(homeless)
+                dbUser.child(email).child("isDisabled").setValue(disabled)
+
+                AlertDialog.Builder(this).apply {
+                    setMessage(getString(R.string.register_success))
+                    setTitle("Register")
+                    setPositiveButton(getString(R.string.login)) { _, _ ->
+                        startActivity(Intent(this@RegisterActivity, LoginActivity::class.java))
+                        finish()
+                    }
+                    show()
+                }
             }
         }
+
         binding.tvLogin.setOnClickListener {
             startActivity(Intent(this@RegisterActivity, LoginActivity::class.java))
             finish()
