f2c454956480f13d0a564cbf209c02021a896cdb
abayup14 <abayup1498@gmail.com>
2023-12-13 06:40:32 +0700

    Update API

--- Cloud Computing/GraphQL/API.py
+++ Cloud Computing/GraphQL/API.py
@@ -4,19 +4,36 @@ import json
 # GraphQL endpoint
 graphql_endpoint = 'http://127.0.0.1:5000/graphql'  # Replace with your actual GraphQL endpoint
 
-# GraphQL query or mutation
-# graphql_query = '''
-# {
-#     query: {listPosts {success\nerrors\nposts\n{id\ntitle\ndescription\ncreated_at}}}
-# }
+# graphql_query='''
+#   mutation {
+#     createUser(nama:"test",email:"test",password:"test",nomor_telepon:"test",
+#     tgl_lahir:"2022-2-2",nik:"test",pengalaman:1,pengalaman_pro:1,edukasi:"Undergraduate",
+#     url_photo:"test",deskripsi:"test",stream:"Spesialisasi") {
+#       user {
+#         nama
+#         email
+#         password
+#         nomor_telepon
+#         tgl_lahir
+#         nik
+#         pengalaman
+#         pengalaman_pro
+#         edukasi
+#         url_photo
+#         deskripsi
+#         stream
+#       }
+#       errors
+#     } 
+#   }
 # '''
-
 graphql_query='''
-  mutation {
-    createUser(nama:"test",email:"test",password:"test",nomor_telepon:"test",
-    tgl_lahir:"2022-2-2",nik:"test",pengalaman:1,pengalaman_pro:1,edukasi:"Undergraduate",
-    url_photo:"test",deskripsi:"test",stream:"Spesialisasi") {
-      user {
+  query {
+    listUsers {
+      success
+      errors
+      users{
+        iduser
         nama
         email
         password
@@ -30,47 +47,9 @@ graphql_query='''
         deskripsi
         stream
       }
-      errors
-    } 
+    }
   }
 '''
-# graphql_query='''
-#   query {
-#     listUsers {
-#       success
-#       errors
-#       users{
-#         iduser
-#         nama
-#         email
-#         password
-#         nomor_telepon
-#         usercol
-#         tgl_lahir
-#         nik
-#         pengalaman
-#         pengalaman_pro
-#         edukasi
-#         url_photo
-#         deskripsi
-#         stream
-#       }
-#     }
-#   }
-# '''
-#   query {
-#     listPosts {
-#       success
-#       errors
-#       posts {
-#         id
-#         title
-#         description
-#         created_at
-#       }
-#     }
-#   }
-# '''
 
 # Headers (optional, include if needed)
 headers = {
