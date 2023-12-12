cddbeffca07405fa8d9cb933ce4d85a809885bc8
geovalenn <90602523+geovalenn@users.noreply.github.com>
2023-12-12 18:11:24 +0700

    Update mutations.py
    update

--- Cloud Computing/GraphQL/api/mutations.py
+++ Cloud Computing/GraphQL/api/mutations.py
@@ -3,62 +3,98 @@ from ariadne import convert_kwargs_to_snake_case
 from api import db
 from api.models import Post
 
+# mutations.py
+from datetime import datetime
+from ariadne import convert_kwargs_to_snake_case
+from api import db
+from api.models import User, Company, Lowongan, Skills, UserHasSkills, SkillsDibutuhkan, Apply, Notifikasi
+
 @convert_kwargs_to_snake_case
-def create_post_resolver(obj, info, title, description):
+def create_user_resolver(obj, info, input):
     try:
-        today = datetime.today().date()
-        post = Post(  
-            title=title,
-            description=description,
-            created_at=today.strftime("%Y-%m-%d")
-        )
-        db.session.add(post)
+        user_data = input.get('user')
+        user = User(**user_data)
+        db.session.add(user)
         db.session.commit()
         payload = {
             "success": True,
-            "post": post.to_dict()
+            "user": user.to_dict()
         }
     except ValueError:
         payload = {
             "success": False,
-            "errors": [f"Incorrect date format provided. "
-            f"Date should be in the format dd-mm-yyyy"]
+            "errors": ["Incorrect data provided."]
         }
     return payload
 
 @convert_kwargs_to_snake_case
-def update_post_resolver(obj, info, id, title, description):
+def create_company_resolver(obj, info, input):
     try:
-        post = Post.query.get(id)  
-        if post:
-            post.title = title
-            post.description = description
-        db.session.add(post)
+        company_data = input.get('company')
+        company = Company(**company_data)
+        db.session.add(company)
         db.session.commit()
         payload = {
             "success": True,
-            "post": post.to_dict()
+            "company": company.to_dict()
         }
-    except AttributeError:
+    except ValueError:
         payload = {
             "success": False,
-            "errors": [f"item matching {id} not found"]
+            "errors": ["Incorrect data provided."]
         }
     return payload
 
 @convert_kwargs_to_snake_case
-def delete_post_resolver(obj, info, id):
+def create_lowongan_resolver(obj, info, input):
     try:
-        post = Post.query.get(id)  
-        db.session.delete(post)
+        lowongan_data = input.get('lowongan')
+        lowongan = Lowongan(**lowongan_data)
+        db.session.add(lowongan)
         db.session.commit()
         payload = {
             "success": True,
-            "post": post.to_dict()
+            "lowongan": lowongan.to_dict()
         }
-    except AttributeError:
+    except ValueError:
         payload = {
             "success": False,
-            "errors": ["Not found"]
+            "errors": ["Incorrect data provided."]
+        }
+    return payload
+
+@convert_kwargs_to_snake_case
+def create_skills_resolver(obj, info, input):
+    try:
+        skills_data = input.get('skills')
+        skills = Skills(**skills_data)
+        db.session.add(skills)
+        db.session.commit()
+        payload = {
+            "success": True,
+            "skills": skills.to_dict()
+        }
+    except ValueError:
+        payload = {
+            "success": False,
+            "errors": ["Incorrect data provided."]
+        }
+    return payload
+
+@convert_kwargs_to_snake_case
+def create_user_has_skills_resolver(obj, info, input):
+    try:
+        user_has_skills_data = input.get('user_has_skills')
+        user_has_skills = UserHasSkills(**user_has_skills_data)
+        db.session.add(user_has_skills)
+        db.session.commit()
+        payload = {
+            "success": True,
+            "user_has_skills": user_has_skills.to_dict()
+        }
+    except ValueError:
+        payload = {
+            "success": False,
+            "errors": ["Incorrect data provided."]
         }
     return payload
