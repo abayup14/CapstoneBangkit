0f0f3c2ad69396f34feab0cb9018046c2e07152a
geovalenn <90602523+geovalenn@users.noreply.github.com>
2023-12-12 18:18:43 +0700

    Update queries.py
    update terbaru

--- Cloud Computing/GraphQL/api/queries.py
+++ Cloud Computing/GraphQL/api/queries.py
@@ -3,13 +3,12 @@ from ariadne import convert_kwargs_to_snake_case
 
 
 @convert_kwargs_to_snake_case
-def list_posts_resolver(obj, info):
+def list_users_resolver(obj, info):
     try:
-        posts = [post.to_dict() for post in Post.query.filter(Post.description.like("%%")).all()]
-        print(posts)
+        users = [user.to_dict() for user in User.query.all()]
         payload = {
             "success": True,
-            "posts": posts
+            "users": users
         }
     except Exception as e:
         payload = {
@@ -18,18 +17,62 @@ def list_posts_resolver(obj, info):
         }
     return payload
 
-
 @convert_kwargs_to_snake_case
-def get_post_resolver(obj, info, id):
+def list_companies_resolver(obj, info):
     try:
-        post = Post.query.get(id)
+        companies = [company.to_dict() for company in Company.query.all()]
         payload = {
             "success": True,
-            "post": post.to_dict()
+            "companies": companies
         }
-    except AttributeError:
+    except Exception as e:
         payload = {
             "success": False,
-            "errors": [f"Post item matching {id} not found"]
+            "errors": [str(e)]
+        }
+    return payload
+
+@convert_kwargs_to_snake_case
+def list_lowongans_resolver(obj, info):
+    try:
+        lowongans = [lowongan.to_dict() for lowongan in Lowongan.query.all()]
+        payload = {
+            "success": True,
+            "lowongans": lowongans
+        }
+    except Exception as e:
+        payload = {
+            "success": False,
+            "errors": [str(e)]
+        }
+    return payload
+
+@convert_kwargs_to_snake_case
+def list_skills_resolver(obj, info):
+    try:
+        skills = [skill.to_dict() for skill in Skills.query.all()]
+        payload = {
+            "success": True,
+            "skills": skills
+        }
+    except Exception as e:
+        payload = {
+            "success": False,
+            "errors": [str(e)]
+        }
+    return payload
+
+@convert_kwargs_to_snake_case
+def list_user_has_skills_resolver(obj, info):
+    try:
+        user_has_skills = [uhs.to_dict() for uhs in UserHasSkills.query.all()]
+        payload = {
+            "success": True,
+            "user_has_skills": user_has_skills
+        }
+    except Exception as e:
+        payload = {
+            "success": False,
+            "errors": [str(e)]
         }
     return payload
