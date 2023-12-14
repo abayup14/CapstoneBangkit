cb05b24933937a3223ea7d83e366e8802f3dcb02
geovalenn <90602523+geovalenn@users.noreply.github.com>
2023-12-14 10:14:09 +0700

    Update __init__.py
    update

--- Cloud Computing/GraphQL/api/__init__.py
+++ Cloud Computing/GraphQL/api/__init__.py
@@ -2,19 +2,21 @@ from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
-
 app = Flask(__name__)
 CORS(app)
-
-# jangan lupa diganti format mysqlnya
-# Format: "mysql+pymysql://username:password@host:port/database_name"
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:@localhost:3306/haire"
+''''
+# Import routes di sini setelah app diinisialisasi
+from app import routes  
+'''
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:mysql@mysql:3306/hAIre"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["SQLALCHEMY_ECHO"] = True
 db = SQLAlchemy(app)
 app.app_context().push()
-
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
