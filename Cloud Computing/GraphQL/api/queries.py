8915fed2578c2adda3e6760f6333447c7598105f
abayup14 <abayup1498@gmail.com>
2023-12-16 18:47:28 +0700

    Update queries.py

--- Cloud Computing/GraphQL/api/queries.py
+++ Cloud Computing/GraphQL/api/queries.py
@@ -124,22 +124,22 @@ def list_skills_resolver(obj, info):
             "errors": [str(e)]
         }
     return payload
-'''
-@convert_kwargs_to_snake_case
-def list_user_has_skills_resolver(obj, info):
-    try:
-        user_has_skills = [uhs.to_dict() for uhs in UserHasSkills.query.all()]
-        payload = {
-            "success": True,
-            "user_has_skills": user_has_skills
-        }
-    except Exception as e:
-        payload = {
-            "success": False,
-            "errors": [str(e)]
-        }
-    return payload
-'''
+
+# @convert_kwargs_to_snake_case
+# def list_user_has_skills_resolver(obj, info):
+#     try:
+#         user_has_skills = [uhs.to_dict() for uhs in UserHasSkills.query.all()]
+#         payload = {
+#             "success": True,
+#             "user_has_skills": user_has_skills
+#         }
+#     except Exception as e:
+#         payload = {
+#             "success": False,
+#             "errors": [str(e)]
+#         }
+#     return payload
+
 
 # @convert_kwargs_to_snake_case
 # def list_posts_resolver(obj, info):
@@ -187,52 +187,52 @@ def list_user_has_skills_resolver(obj, info):
 #             "errors": [f"Post item matching {id} not found"]
 #         }
 #     return payload
-'''
-@convert_kwargs_to_snake_case
-def list_companies_resolver(obj, info):
-    try:
-        companies = [company.to_dict() for company in Company.query.all()]
-        payload = {
-            "success": True,
-            "companies": companies
-        }
-    except Exception as e:
-        payload = {
-            "success": False,
-            "errors": [str(e)]
-        }
-    return payload
 
-@convert_kwargs_to_snake_case
-def list_lowongans_resolver(obj, info):
-    try:
-        lowongans = [lowongan.to_dict() for lowongan in Lowongan.query.all()]
-        payload = {
-            "success": True,
-            "lowongans": lowongans
-        }
-    except Exception as e:
-        payload = {
-            "success": False,
-            "errors": [str(e)]
-        }
-    return payload
+# @convert_kwargs_to_snake_case
+# def list_companies_resolver(obj, info):
+#     try:
+#         companies = [company.to_dict() for company in Company.query.all()]
+#         payload = {
+#             "success": True,
+#             "companies": companies
+#         }
+#     except Exception as e:
+#         payload = {
+#             "success": False,
+#             "errors": [str(e)]
+#         }
+#     return payload
 
-@convert_kwargs_to_snake_case
-def list_skills_resolver(obj, info):
-    try:
-        skills = [skill.to_dict() for skill in Skills.query.all()]
-        payload = {
-            "success": True,
-            "skills": skills
-        }
-    except Exception as e:
-        payload = {
-            "success": False,
-            "errors": [str(e)]
-        }
-    return payload
-'''
+# @convert_kwargs_to_snake_case
+# def list_lowongans_resolver(obj, info):
+#     try:
+#         lowongans = [lowongan.to_dict() for lowongan in Lowongan.query.all()]
+#         payload = {
+#             "success": True,
+#             "lowongans": lowongans
+#         }
+#     except Exception as e:
+#         payload = {
+#             "success": False,
+#             "errors": [str(e)]
+#         }
+#     return payload
+
+# @convert_kwargs_to_snake_case
+# def list_skills_resolver(obj, info):
+#     try:
+#         skills = [skill.to_dict() for skill in Skills.query.all()]
+#         payload = {
+#             "success": True,
+#             "skills": skills
+#         }
+#     except Exception as e:
+#         payload = {
+#             "success": False,
+#             "errors": [str(e)]
+#         }
+#     return payload
+
 # @convert_kwargs_to_snake_case
 # def list_user_has_skills_resolver(obj, info):
 #     try:
